6af91a91229d21cb17a1292ae751060628d46763
Micah Johnston <micahrjohnston@gmail.com>
2026-01-13 10:18:40 -0600

    appkit: tighten scope of unsafe blocks in surface module

--- src/backend/app_kit/surface.rs
+++ src/backend/app_kit/surface.rs
@@ -23,12 +23,16 @@ const kIOSurfaceSuccess: kern_return_t = 0;
 
 const BYTES_PER_ELEMENT: usize = 4;
 
-unsafe fn set_contents_opaque(layer: &CALayer, contents_opaque: bool) {
-    let () = msg_send![layer, setContentsOpaque: contents_opaque];
+fn set_contents_opaque(layer: &CALayer, contents_opaque: bool) {
+    unsafe {
+        let () = msg_send![layer, setContentsOpaque: contents_opaque];
+    }
 }
 
-unsafe fn set_contents_changed(layer: &CALayer) {
-    let () = msg_send![layer, setContentsChanged];
+fn set_contents_changed(layer: &CALayer) {
+    unsafe {
+        let () = msg_send![layer, setContentsChanged];
+    }
 }
 
 pub struct Surface {
@@ -40,63 +44,64 @@ pub struct Surface {
 
 impl Surface {
     pub fn new(width: usize, height: usize) -> Result<Surface> {
+        let properties = CFDictionary::<CFString, CFNumber>::from_slices(
+            &[
+                unsafe { kIOSurfaceWidth },
+                unsafe { kIOSurfaceHeight },
+                unsafe { kIOSurfaceBytesPerElement },
+                unsafe { kIOSurfacePixelFormat },
+            ],
+            &[
+                &CFNumber::new_i32(width as i32),
+                &CFNumber::new_i32(height as i32),
+                &CFNumber::new_i32(BYTES_PER_ELEMENT as i32),
+                &CFNumber::new_i32(kCVPixelFormatType_32BGRA as i32),
+            ],
+        );
+
+        let Some(surface) = (unsafe { IOSurfaceRef::new(properties.as_opaque()) }) else {
+            return Err(Error::Os(OsError::Other("could not create IOSurface")));
+        };
+
         unsafe {
-            let properties = CFDictionary::<CFString, CFNumber>::from_slices(
-                &[
-                    kIOSurfaceWidth,
-                    kIOSurfaceHeight,
-                    kIOSurfaceBytesPerElement,
-                    kIOSurfacePixelFormat,
-                ],
-                &[
-                    &CFNumber::new_i32(width as i32),
-                    &CFNumber::new_i32(height as i32),
-                    &CFNumber::new_i32(BYTES_PER_ELEMENT as i32),
-                    &CFNumber::new_i32(kCVPixelFormatType_32BGRA as i32),
-                ],
-            );
-
-            let Some(surface) = IOSurfaceRef::new(properties.as_opaque()) else {
-                return Err(Error::Os(OsError::Other("could not create IOSurface")));
-            };
-
             surface.set_value(kIOSurfaceColorSpace, kCGColorSpaceSRGB);
-
-            let layer = CALayer::layer();
-            let surface_ptr = CFRetained::as_ptr(&surface).as_ptr();
-            layer.setContents(Some(&*(surface_ptr as *const AnyObject)));
-            layer.setOpaque(true);
-            set_contents_opaque(&layer, true);
-            layer.setContentsGravity(kCAGravityBottomLeft);
-            layer.setMagnificationFilter(kCAFilterNearest);
-
-            Ok(Surface {
-                layer,
-                surface,
-                width,
-                height,
-            })
         }
+
+        let layer = CALayer::layer();
+        let surface_ptr = CFRetained::as_ptr(&surface).as_ptr();
+        unsafe {
+            layer.setContents(Some(&*(surface_ptr as *const AnyObject)));
+        }
+        layer.setOpaque(true);
+        set_contents_opaque(&layer, true);
+        layer.setContentsGravity(unsafe { kCAGravityBottomLeft });
+        layer.setMagnificationFilter(unsafe { kCAFilterNearest });
+
+        Ok(Surface {
+            layer,
+            surface,
+            width,
+            height,
+        })
     }
 
     pub fn with_buffer<F: FnOnce(&mut [u32])>(&mut self, f: F) {
+        let ret = unsafe { self.surface.lock(IOSurfaceLockOptions::empty(), ptr::null_mut()) };
+        if ret != kIOSurfaceSuccess {
+            return;
+        }
+
+        let addr = self.surface.base_address().as_ptr();
+        let len = self.width * self.height;
+        let buffer = unsafe { slice::from_raw_parts_mut(addr as *mut u32, len) };
+        f(buffer);
+
         unsafe {
-            let ret = self.surface.lock(IOSurfaceLockOptions::empty(), ptr::null_mut());
-            if ret != kIOSurfaceSuccess {
-                return;
-            }
-
-            let addr = self.surface.base_address().as_ptr();
-            let buffer = slice::from_raw_parts_mut(addr as *mut u32, self.width * self.height);
-            f(buffer);
-
             self.surface.unlock(IOSurfaceLockOptions::empty(), ptr::null_mut());
         }
     }
 
     pub fn present(&self) {
-        unsafe {
-            set_contents_changed(&self.layer);
-        }
+        set_contents_changed(&self.layer);
     }
 }
